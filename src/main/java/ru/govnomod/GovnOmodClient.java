a811d2fc1200a80c578dd4e9be71a41d05b5c2f7
1nTheF0rest <kolya.zatsep@gmail.com>
2026-09-20 17:28:24 +0300

    Rework govnOmod: keybinds, menu and Funya metadata

--- src/main/java/ru/govnomod/GovnOmodClient.java
+++ src/main/java/ru/govnomod/GovnOmodClient.java
@@ -1,13 +1,56 @@
 package ru.govnomod;
 
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.KeyMapping;
+import org.lwjgl.glfw.GLFW;
 
 public final class GovnOmodClient implements ClientModInitializer {
     public static final String MOD_ID = "govnomod";
     public static final Config CONFIG = Config.load();
 
+    private static final String KEY_CATEGORY = "key.categories.govnomod";
+
+    public static KeyMapping TOGGLE_KEY;
+    public static KeyMapping MENU_KEY;
+
     @Override
     public void onInitializeClient() {
+        TOGGLE_KEY = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.govnomod.toggle",
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_G,
+                KEY_CATEGORY
+        ));
+
+        MENU_KEY = KeyBindingHelper.registerKeyBinding(new KeyMapping(
+                "key.govnomod.menu",
+                InputConstants.Type.KEYSYM,
+                GLFW.GLFW_KEY_RIGHT_SHIFT,
+                KEY_CATEGORY
+        ));
+
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (TOGGLE_KEY.consumeClick()) {
+                CONFIG.enabled = !CONFIG.enabled;
+                CONFIG.save();
+                if (client.player != null) {
+                    client.player.displayClientMessage(
+                            net.minecraft.network.chat.Component.literal(
+                                    "Говномод: " + (CONFIG.enabled ? "ВКЛЮЧЕН" : "ВЫКЛЮЧЕН")
+                            ),
+                            true
+                    );
+                }
+            }
+
+            while (MENU_KEY.consumeClick()) {
+                client.setScreen(new GovnOmodScreen(client.screen));
+            }
+        });
+
         CONFIG.save();
     }
 }
